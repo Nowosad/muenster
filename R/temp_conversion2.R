852d465c568ebdff83437b906602d8fa4c12bc30
Jakub Nowosad <tupiszakaczki@gmail.com>
2026-04-29 15:41:40 +0200

    added example data

--- R/temp_conversion2.R
+++ R/temp_conversion2.R
@@ -13,6 +13,7 @@
 #' temp_conversion(75)
 #' temp_conversion(110)
 #' temp_conversion(0)
+#' temp_conversion(pollen_count$birch)
 temp_conversion2 = function(temp_f) {
   result = (temp_f - 32) / 1.8
   result_tb = tibble::tibble(temp_f = temp_f, temp_c = result)
